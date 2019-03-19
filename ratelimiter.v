ca374eb2e56679b91bb526464bd29c8d5b72eeef
Lance <lance.oribello@mail.utoronto.ca>
2019-03-18 21:29:19 -0400

    newerrl

--- ratelimiter.v
+++ ratelimiter.v
@@ -1,5 +1,6 @@
-module RateDivider(in_clock, out_clock);
+module RateDivider(in_clock, reset_val, out_clock);
 	input in_clock;
+	input [24:0] reset_val;
 	reg [24:0] counter;
 	output reg out_clock;
 	
@@ -12,11 +13,27 @@ module RateDivider(in_clock, out_clock);
 	begin
 		if(counter == 0)
 			begin
-				counter <= 25'd49999999;//reset the clock
+				counter <= reset_val;
 				out_clock <= ~out_clock;
 			end
 		else begin
-			counter <= counter - 25'd1;//decrement
+			counter <= counter - 25'd1;
 		end
 	end
 endmodule	
+
+module caseSelect(s, out);
+    input [1:0] s;
+    output reg [24:0] out; 
+	
+	always @(*)
+	begin
+		case(s[1:0])
+			2'b00: out = 25'd49999999;
+			2'b01: out = 25'd24999999;
+			2'b10: out = 25'd12499999;
+			2'b11: out = 25'd6249999;
+			default: out = 0;
+		endcase
+	end
+endmodule
